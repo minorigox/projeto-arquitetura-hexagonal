37f69cf9d6042eb4b224d1b46299c7b00f52d401
Igor Minoru Tamura <igor.minoru@outlook.com>
2025-01-31 10:05:57 -0300

    Testando o adapter CLI.

--- adapters/cli/product.go
+++ adapters/cli/product.go
@@ -1,11 +1,11 @@
-package main
+package cli
 
 import (
 	"fmt"
-	"application"
+	"github.com/minorigox/projeto-arquitetura-hexagonal/application"
 )
 
-func Run(service application.ProductServiceInterface, action string, productId string, productName string, productPrice float) (string, error) {
+func Run(service application.ProductServiceInterface, action string, productId string, productName string, productPrice float64) (string, error) {
 	var result = ""
 	switch action {
 		case "create":
@@ -21,7 +21,7 @@ func Run(service application.ProductServiceInterface, action string, productId s
 			if (err != nil) {
 				return result, err
 			}
-			res, err = service.Enable(product)
+			res, err := service.Enable(product)
 			if (err != nil) {
 				return result, err
 			}
@@ -31,7 +31,7 @@ func Run(service application.ProductServiceInterface, action string, productId s
 			if (err != nil) {
 				return result, err
 			}
-			res, err = service.Disable(product)
+			res, err := service.Disable(product)
 			if (err != nil) {
 				return result, err
 			}
---
+++ adapters/cli/product_test.go
@@ -0,0 +1,60 @@
+package cli_test
+
+import (
+	"testing"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"github.com/golang/mock/gomock"
+	"github.com/minorigox/projeto-arquitetura-hexagonal/adapters/cli"
+	mock_application "github.com/minorigox/projeto-arquitetura-hexagonal/application/mocks"
+)
+
+func TestRun(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	productId := "abc"
+	productName := "Product Test"
+	productPrice := 10.00
+	productStatus := "enabled"
+
+	productMock := mock_application.NewMockProductInterface(ctrl)
+	productMock.EXPECT().GetId().Return(productId).AnyTimes()
+	productMock.EXPECT().GetName().Return(productName).AnyTimes()
+	productMock.EXPECT().GetPrice().Return(productPrice).AnyTimes()
+	productMock.EXPECT().GetStatus().Return(productStatus).AnyTimes()
+
+	service := mock_application.NewMockProductServiceInterface(ctrl)
+	service.EXPECT().Create(productName, productPrice).Return(productMock, nil).AnyTimes()
+	service.EXPECT().Get(productId).Return(productMock, nil).AnyTimes()
+	service.EXPECT().Enable(productMock).Return(productMock, nil).AnyTimes()
+	service.EXPECT().Disable(productMock).Return(productMock, nil).AnyTimes()
+
+	//Testando create
+	resultExpected := fmt.Sprintf(
+		"Product ID %s with the name %s has been created with the price %f and status %s.", 
+		productId, productName, productPrice, productStatus)
+
+	result, err := cli.Run(service, "create", "", productName, productPrice)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+	
+	//Testando enable
+	resultExpected = fmt.Sprintf("Product %s has been enabled.", productName)
+	result, err = cli.Run(service, "enable", productId, "", 0)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+
+	//Testando disable
+	resultExpected = fmt.Sprintf("Product %s has been disabled.", productName)
+	result, err = cli.Run(service, "disable", productId, "", 0)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+
+	//Testando get
+	resultExpected = fmt.Sprintf("Product ID: %s\nName: %s\nPrice: %f\nStatus: %s",
+		productId, productName, productPrice, productStatus)
+	result, err = cli.Run(service, "get", productId, "", 0)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+}
